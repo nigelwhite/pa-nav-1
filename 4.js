752006d86986bec820dcccad4555bb713fc6e257
nigelwhite <webdev@permaculture.org.uk>
2021-07-02 07:51:14 +0100

    4 has bugs

---
+++ 4.js
@@ -0,0 +1,10 @@
+(function mainScript() {
+	'use strict';
+	const offcanvasToggle = document.querySelector(
+		'[data-bs-toggle="offcanvas"]'
+	);
+	const offcanvasCollapse = document.querySelector('.offcanvas-collapse');
+	offcanvasToggle.addEventListener('click', function() {
+		offcanvasCollapse.classList.toggle('open');
+	});
+})();
